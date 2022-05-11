90a9e6c713a346dd04c30d7a010d9a8123badeff
bellmont92 <59203307+bellmont92@users.noreply.github.com>
2022-05-11 06:52:20 +0100

    update to e14

--- exercise_14.py
+++ exercise_14.py
@@ -1,21 +1,34 @@
 
 
-person_0 = {
-    'first_name': 'laura', 
-    'last_name': 'palmer', 
-    'age': 21, 'city': 
-    'twin peaks'
-    },
-person_1 = {
-    'first_name': 'tom', 
-    'last_name': 'bellmont', 
-    'age': 29, 
-    'city': 'bristol'
+# person_0 = {
+#     'first_name': 'laura', 
+#     'last_name': 'palmer', 
+#     'age': 21, 'city': 
+#     'twin peaks'
+#     }
+# person_1 = {
+#     'first_name': 'tom', 
+#     'last_name': 'bellmont', 
+#     'age': 29, 
+#     'city': 'bristol'
+#     }
+# person_2 = {
+#     'first_name': 'danielle', 
+#     'last_name': 'batten', 
+#     'age': 27, 
+#     'city': 'bristol'
+#     }
+    
+# people = [person_0, person_1, person_2]
+
+# for person in people:
+#     print(person)
+
+favoruite_places = {
+    'tom': 'india', 
+    'ross': 'wales', 
+    'rosie': 'peru',
     }
     
-people = [person_0, person_1]
-
-for person in people:
-    for column, value in person.items():
-        full_name =  person['first_name'] + " " + person['last_name']
-        print(full_name)
+for person, place in favoruite_places.items():
+    print(person.title() + "'s favourite place is " + place.title() + ".")
